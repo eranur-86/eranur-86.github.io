44e9609f6b76808d8d382d92c6aeb398ab1188b5
Thomas <70510595+eranur-86@users.noreply.github.com>
2021-09-25 05:16:59 +0200

    added prototype for LandingPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,27 @@
 import React, { Suspense } from "react";
 import {NavElement} from "./navigation/navbar"
-import { Container, Row, Col } from "react-bootstrap";
+import { Container} from "react-bootstrap";
 import { Route, Switch } from "react-router-dom";
 import { HOME, PROJEKTE, SKILLS } from "./navigation/navgoals";
-import ProjektPage from "./projekte/projektPage";
 
 const App = () => {
 
+  const LandingPage = React.lazy(() => import('./landingPage'));
   const SkillPage = React.lazy(() => import('./skills/skillPage'));
-  //const ProjektPage = React.lazy(() => import('./projekte/projektPage'));
+  const ProjektPage = React.lazy(() => import('./projekte/projektPage'));
 
   return (
     <div>
       <NavElement/>
       <Suspense fallback={
       <div>
-        <NavElement/>
         <div>Loading...</div>
       </div>}>
       <Container fluid>
         <Switch>
           <Route path={SKILLS}><SkillPage/></Route>
           <Route path={PROJEKTE}><ProjektPage/></Route>
-          <Route path={HOME} exact></Route>
+          <Route path={HOME} exact><LandingPage/></Route>
         </Switch>
       </Container>
       </Suspense>
---
+++ src/landingPage.tsx
@@ -0,0 +1,13 @@
+const LandingPage = () => {
+    return (
+        <div>
+            <h2>
+                Willkommen auf meiner Homepage.
+            </h2>
+            <div>Diese Seite befindet sich aktuell noch im Aufbau.</div>
+            <div>Die Navigationsitems funktionieren bereits, jedoch sind alle Seiten "work in progress".</div>
+        </div>
+    )
+}
+
+export default LandingPage;
